63e44f196ad72233abf5a58519cb202ab6dd9744
diimartell <ruanvitoraquinooliveira@gmail.com>
2024-10-28 20:52:24 -0300

    .

--- task/urls.py
+++ task/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
      path('', views.task_list, name='task_list'),
-     path('add/', views.add_task, name='add_task' )
+     path('add/', views.add_task, name='add_task' ),
+     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
