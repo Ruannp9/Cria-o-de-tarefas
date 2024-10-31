51ee8c0876bb1cb030b2c201925ec97654d6b1d6
diimartell <ruanvitoraquinooliveira@gmail.com>
2024-10-31 18:58:26 -0300

    Atualizado da aula passada

--- task/urls.py
+++ task/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
      path('task/', views.task_list, name='task_list'),
      path('add/', views.add_task, name='add_task' ),
-     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),    
+     path('delete/<int:Aluno_id>/', views.delete_cadastro, name='delete_cadastro'),
      path('', views.cadastro_escola, name='cadastro_escola'),
      path('ad/', views.cadastro_adicionar, name='cadastro_adicionar' ),
+     path('atualizar_cadastro/<int:Aluno_id>/', views.atualizar_cadastro, name='atualizar_cadastro'),
 ]
--- task/views.py
+++ task/views.py
@@ -19,16 +19,41 @@ def delete_task(request, task_id):
     return redirect('task_list')
 # Create your views here.
 
+
+
 def cadastro_escola(request):
     escola = Aluno.objects.all()
     return render (request, 'escola/cadastro_escola.html', {'escola': escola})
 
 def cadastro_adicionar(request):
     if request.method == 'POST':
-        cadastrar = request.POST.get('cadastrar')
-        if cadastrar:
-            Aluno.objects.create(nome=cadastrar)
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        print(nome)
+        print(email)
+        if nome:
+            Aluno.objects.create(nome=nome,email=email)
+            
             return redirect('cadastro_escola')
     return render(request, 'escola/cadastro_adicionar.html')
+
+def delete_cadastro(request, Aluno_id):
+    cadastro = Aluno.objects.get(id=Aluno_id)
+    cadastro.delete()
+    return redirect('cadastro_escola')
             
-            
+from django.shortcuts import get_object_or_404
+def atualizar_cadastro(request, Aluno_id):
+    
+    i = get_object_or_404(Aluno, id=Aluno_id)
+    
+    if request.method == 'POST':
+        
+     i.nome = request.POST.get('nome')
+     i.email = request.POST.get('email')
+     
+     
+     i.save()
+     
+     return redirect('cadastro_escola')
+    return render(request, 'escola/cadastro_editar.html', {'Cadastro':i})
